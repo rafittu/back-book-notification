cfa1eba26dde562f23cc5f7143c971ea4ef864b8
rafittu <rafittu@outlook.com>
2024-12-03 13:41:37 -0300

    :tada: start new project

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	AWSRegion    string
+	AWSBucket    string
+	SNSTopicARN  string
+}
+
+func LoadConfig() Config {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Fatalf("Failed to load .env: %v", err)
+	}
+
+	return Config{
+		AWSRegion:   os.Getenv("AWS_REGION"),
+		AWSBucket:   os.Getenv("AWS_BUCKET"),
+		SNSTopicARN: os.Getenv("AWS_SNS_TOPIC_ARN"),
+	}
+}
